e9df0d05b626adfa2cc7b34b4d224784dbe7676b
brkcvn <burakcivan22@gmail.com>
2023-04-09 14:26:43 +0300

    external auth control added

--- src/components/_externalAuth.tsx
+++ src/components/_externalAuth.tsx
@@ -1,11 +1,20 @@
+import { useSession, signIn } from "next-auth/react";
+
 export default function ExternalAuth() {
+    const { data: session } = useSession();
+    debugger;
+
     return (
         <div>
             <hr></hr>
 
             <div className="space-y-3">
-                <button className="w-full py-2 px-4 rounded-lg bg-gray-500 text-white hover:bg-gray-700">Sign In with Discord</button>
-                <button className="w-full py-2 px-4 rounded-lg bg-gray-500 text-white hover:bg-gray-700">Sign In with Google</button>
+                <button
+                    onClick={() => signIn()}
+                    className="w-full py-2 px-4 rounded-lg bg-gray-500 text-white hover:bg-gray-700"
+                >
+                    Sign In with Google
+                </button>
             </div>
         </div>
     )
